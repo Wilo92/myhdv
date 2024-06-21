9b5b2a860446f8fa3830f4ac4b39b101c559003d
Wilo92 <107453564+Wilo92@users.noreply.github.com>
2024-06-20 21:58:18 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+/*efecto seccion estucios, expereincias, supervisiones---*/
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+
+
+    event.currentTarget.classList.add("active-link");
+
+    document.getElementById(tabname).classList.add("active-tab");
+
+}
+
+
+
+/*CSS PARA PONER LA BARRA DE MENU EN RESPONSIVO CON MOVIMIENTO*/
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+    sidemenu.style.right = "0";
+
+}
+
+function closemenu() {
+    sidemenu.style.right = "-200px";
+
+}
